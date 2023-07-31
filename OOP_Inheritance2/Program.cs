a6a2025a877d62357fd01b47af429c3c768c13c1
ÖmerAydoğan <96176210+OmerAydogan91@users.noreply.github.com>
2023-07-31 10:31:20 +0300

    Add project files.

---
+++ OOP_Inheritance2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace OOP_Inheritance2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //oop örneği olan Inheritance/kalıtım miras alma konusunu ile ilgili örnek anlatım gerçekleştirdik.
+            InheritanceMethod();
+        }
+        private static void InheritanceMethod()
+        {//Bu örnekte, Çalışan sınıfı temel sınıf olarak kullanılmış ve Yönetici sınıfı, Çalışan sınıfından miras alınmıştır. Yönetici sınıfı, Çalışan sınıfının özelliklerini ve metotlarını kullanır ve aynı zamanda kendine ait olan SorumluOlduğuAlan özelliği ve ZamYap() metodu vardır.
+         //Programı çalıştırdığınızda, çalışanın ve yöneticinin bilgileri yazdırılacak, sonra yöneticiye zam yapıldıktan sonra güncel maaş bilgisi gösterilecektir. Bu örnek, kalıtımın daha somut bir kullanımını göstermektedir.
+            
+            // Çalışan nesnesi oluşturuyoruz
+            Çalışan çalışan1 = new Çalışan();
+            çalışan1.Ad = "Ali";
+            çalışan1.Departman = "IT";
+            çalışan1.Maaş = 5000;
+
+            // Yönetici nesnesi oluşturuyoruz
+            Yönetici yönetici1 = new Yönetici();
+            yönetici1.Ad = "Ayşe";
+            yönetici1.Departman = "Finans";
+            yönetici1.Maaş = 8000;
+            yönetici1.SorumluOlduğuAlan = "Muhasebe";
+
+            // Çalışan bilgilerini göster
+            Console.WriteLine("Çalışan Bilgileri:");
+            çalışan1.BilgiGöster();
+            Console.WriteLine();
+
+            // Yönetici bilgilerini göster
+            Console.WriteLine("Yönetici Bilgileri:");
+            yönetici1.BilgiGöster();
+            Console.WriteLine("Sorumlu Olduğu Alan: " + yönetici1.SorumluOlduğuAlan);
+            Console.WriteLine();
+
+            // Yöneticiye zam yap
+            double zamYüzdesi = 10;
+            yönetici1.ZamYap(zamYüzdesi);
+
+            // Zam yapıldıktan sonra güncel maaş bilgisi
+            Console.WriteLine("Zam Yapıldıktan Sonra Yönetici Maaşı: " + yönetici1.Maaş);
+
+        }
+    }
+    // Üst sınıf (temel sınıf)
+    class Çalışan
+    {
+        public string Ad { get; set; }
+        public string Departman { get; set; }
+        public double Maaş { get; set; }
+
+        public void BilgiGöster()
+        {
+            Console.WriteLine("Ad: " + Ad);
+            Console.WriteLine("Departman: " + Departman);
+            Console.WriteLine("Maaş: " + Maaş);
+        }
+    }
+
+    // Alt sınıf (türetilmiş sınıf)
+    class Yönetici : Çalışan
+    {
+        public string SorumluOlduğuAlan { get; set; }
+
+        public void ZamYap(double yüzde)
+        {
+            Maaş = Maaş + (Maaş * yüzde / 100);
+        }
+    }
+}
---
+++ OOP_Lesson_Inheritance/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace OOP_Lesson_Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bu örnekte, Hayvan sınıfı temel sınıf olarak kullanılmış ve Kus sınıfı, Hayvan sınıfından miras alınmıştır. Kus sınıfı, Hayvan sınıfının özelliklerini ve metotlarını kullanır ve aynı zamanda kendine ait olan Renk özelliği ve Ucmak() metodu vardır.
+
+            // Programı çalıştırdığınızda, kartalın türü, yaşı ve rengi yazdırılacak, ardından kartalın ses çıkaracağı ve uçacağı ifade edilecektir.Bu, kalıtımın nasıl çalıştığını gösteren temel bir örnektir.
+            // Alt sınıftan bir nesne oluşturuyoruz
+            Kus kartal = new Kus();
+            kartal.Tur = "Kartal";
+            kartal.Yas = 5;
+            kartal.Renk = "Kahverengi";
+
+            // Miras alınan üst sınıfın özelliğini kullanıyoruz
+            Console.WriteLine("Tür: " + kartal.Tur);
+            Console.WriteLine("Yaş: " + kartal.Yas);
+
+            // Alt sınıfa ait özelliği kullanıyoruz
+            Console.WriteLine("Renk: " + kartal.Renk);
+
+            // Miras alınan üst sınıfın metotunu çağırıyoruz
+            kartal.SesCikar();
+
+            // Alt sınıfa ait metodu çağırıyoruz
+            kartal.Ucmak();
+            Console.ReadKey();
+        }
+    }
+    // Üst sınıf (temel sınıf)
+    class Hayvan
+    {
+        public string Tur { get; set; }
+        public int Yas { get; set; }
+
+        public void SesCikar()
+        {
+            Console.WriteLine("Hayvan ses çıkarıyor...");
+        }
+    }
+
+    // Alt sınıf (türetilmiş sınıf)
+    class Kus : Hayvan
+    {
+        public string Renk { get; set; }
+
+        public void Ucmak()
+        {
+            Console.WriteLine("Kuş uçuyor...");
+        }
+    }
+}
